81ba2f42691a931c47236f3584138eec3a98998a
billym711 <bmoorekid@gmail.com>
2024-09-14 22:11:45 -0400

    Calculating species, item, moves

--- RS Checksum Calculator.py
+++ RS Checksum Calculator.py
@@ -27,10 +27,10 @@ original_checksum = ((data1 % 65536) + math.trunc(data1/65536) + (data2 % 65536)
         + (data6 % 65536) + math.trunc(data6/65536) + (data7 % 65536) + math.trunc(data7/65536) + (data8 % 65536)
         + math.trunc(data8/65536) + (data9 % 65536) + math.trunc(data9/65536) + (data10 % 65536) + math.trunc(data10/65536)
         + (data11 % 65536) + math.trunc(data11/65536) + (data12 % 65536) + math.trunc(data12/65536))%65536
-for i in range(3000, 3500):
+for i in range(3000, 100000):
     player_key = pid ^ (literal_eval(f"{int(otids_list[i][2]):#0{6}x}" + f"{int(otids_list[i][1]):#0{6}x}"[2::]))
-    print("Player frame " + str(i-1) + " Player Key: " + str(player_key))
-    for j in range(700, 1200):
+    #print("Player frame " + str(i-1) + " Player Key: " + str(player_key))
+    for j in range(700, 100000):
 
         enemy_key = pid ^ (literal_eval(f"{int(otids_list[j][1]):#0{6}x}" + f"{int(otids_list[j][2]):#0{6}x}"[2::]))
 
@@ -40,29 +40,20 @@ for i in range(3000, 3500):
         + (player_key ^ enemy_key ^ data6 % 65536) + math.trunc((player_key ^ enemy_key ^ data6)/65536) + (player_key ^ enemy_key ^ data7 % 65536) + math.trunc((player_key ^ enemy_key ^ data7)/65536) + (player_key ^ enemy_key ^ data8 % 65536)
         + math.trunc((player_key ^ enemy_key ^ data8)/65536) + (player_key ^ enemy_key ^ data9 % 65536) + math.trunc((player_key ^ enemy_key ^ data9)/65536) + (player_key ^ enemy_key ^ data10 % 65536) + math.trunc((player_key ^ enemy_key ^ data10)/65536)
         + (player_key ^ enemy_key ^ data11 % 65536) + math.trunc((player_key ^ enemy_key ^ data11)/65536) + (player_key ^ enemy_key ^ data12 % 65536) + math.trunc((player_key ^ enemy_key ^ data12)/65536))%65536
-        print("Enemy frame " + str(j) + " Enemy Key: " + str(enemy_key) + " New Checksum: " + str(new_checksum))
+        #print("Enemy frame " + str(j) + " Enemy Key: " + str(enemy_key) + " New Checksum: " + str(new_checksum))
         if new_checksum == original_checksum:
-            valid_combinations.append([i-1, j-1])
-'''
-print("Player OTID: " + f"{int(otids_list[1][2]):#0{6}x}" + f"{int(otids_list[1][1]):#0{6}x}"[2::])
-print("Enemy OTID: " + f"{int(otids_list[1][1]):#0{6}x}" + f"{int(otids_list[1][2]):#0{6}x}"[2::])
-print(literal_eval(f"{int(otids_list[1][1]):#0{6}x}" + f"{int(otids_list[1][2]):#0{6}x}"[2::]))
-print(literal_eval(f"{int(otids_list[1][2]):#0{6}x}" + f"{int(otids_list[1][1]):#0{6}x}"[2::]))
-print(pid ^ (literal_eval(f"{int(otids_list[1][2]):#0{6}x}" + f"{int(otids_list[1][1]):#0{6}x}"[2::])))
-print(pid ^ (literal_eval(f"{int(otids_list[1][1]):#0{6}x}" + f"{int(otids_list[1][2]):#0{6}x}"[2::])))
-
-
-player_key = pid ^ (literal_eval(f"{int(otids_list[1][2]):#0{6}x}" + f"{int(otids_list[1][1]):#0{6}x}"[2::]))
-enemy_key = pid ^ (literal_eval(f"{int(otids_list[1][1]):#0{6}x}" + f"{int(otids_list[1][2]):#0{6}x}"[2::]))
-
-checksum = data1 ^ (pid ^ (literal_eval(f"{int(otids_list[1][2]):#0{6}x}" + f"{int(otids_list[1][1]):#0{6}x}"[2::]))) ^ 979719048
-
-print(checksum)'''
-'''
-print(((979719048 ^ 4218262619 ^ data1 % 65536) + math.trunc((979719048 ^ 4218262619 ^ data1)/65536) + (979719048 ^ 4218262619 ^ data2 % 65536) + math.trunc((979719048 ^ 4218262619 ^ data2)/65536) + (979719048 ^ 4218262619 ^ data3 % 65536)
-        + math.trunc((979719048 ^ 4218262619 ^ data3)/65536) + (979719048 ^ 4218262619 ^ data4 % 65536) + math.trunc((979719048 ^ 4218262619 ^ data4)/65536) + (979719048 ^ 4218262619 ^ data5 % 65536) + math.trunc((979719048 ^ 4218262619 ^ data5)/65536)
-        + (979719048 ^ 4218262619 ^ data6 % 65536) + math.trunc((979719048 ^ 4218262619 ^ data6)/65536) + (979719048 ^ 4218262619 ^ data7 % 65536) + math.trunc((979719048 ^ 4218262619 ^ data7)/65536) + (979719048 ^ 4218262619 ^ data8 % 65536)
-        + math.trunc((979719048 ^ 4218262619 ^ data8)/65536) + (979719048 ^ 4218262619 ^ data9 % 65536) + math.trunc((979719048 ^ 4218262619 ^ data9)/65536) + (979719048 ^ 4218262619 ^ data10 % 65536) + math.trunc((979719048 ^ 4218262619 ^ data10)/65536)
-        + (979719048 ^ 4218262619 ^ data11 % 65536) + math.trunc((979719048 ^ 4218262619 ^ data11)/65536) + (979719048 ^ 4218262619 ^ data12 % 65536) + math.trunc((979719048 ^ 4218262619 ^ data12)/65536))%65536)
-        '''
-print(valid_combinations)
+            #valid_combinations.append([i-1, j-1])
+            #if ("0x" + f"{(int(format((player_key ^ enemy_key ^ data4), '#034b')[2:], 2)):#0{10}x}"[-4:] == '0x6d83' or f"{(int(format((player_key ^ enemy_key ^ data4), '#034b')[2:], 2)):#0{10}x}"[0:6] == '0x6d83' or "0x" + f"{(int(format((player_key ^ enemy_key ^ data5), '#034b')[2:], 2)):#0{10}x}"[-4:] == '0x6d83' or f"{(int(format((player_key ^ enemy_key ^ data5), '#034b')[2:], 2)):#0{10}x}"[0:6] == '0x6d83'):
+            #    print("ACE move found: " + str(i-1) + " " + str(j-1))
+            #if (f"{(int(format((player_key ^ enemy_key ^ data1), '#034b')[2:], 2)):#0{10}x}"[0:6] == '0x0113'):
+            #    print("Eon Ticket found: " + str(i-1) + " " + str(j-1))
+            
+            print("Match Found! Player frame: " + str(i-1) + " Enemy Frame: " + str(j-1) + " Player TID/SID: " + otids_list[i][1] + " " + otids_list[i][2] + 
+                  " Enemy TID/SID: " + otids_list[j][2] + " " + otids_list[j][1] + " Species: " + hex(int(format((player_key ^ enemy_key ^ data1), '#034b')[2:], 2))[0:2] 
+                  + hex(int(format((player_key ^ enemy_key ^ data1), '#034b')[2:], 2))[-4:] 
+                  + " Held Item: " + f"{(int(format((player_key ^ enemy_key ^ data1), '#034b')[2:], 2)):#0{10}x}"[0:6] 
+                  + " Moves: " + "0x" + f"{(int(format((player_key ^ enemy_key ^ data4), '#034b')[2:], 2)):#0{10}x}"[-4:] + " " + f"{(int(format((player_key ^ enemy_key ^ data4), '#034b')[2:], 2)):#0{10}x}"[0:6]
+                  + " 0x" + f"{(int(format((player_key ^ enemy_key ^ data5), '#034b')[2:], 2)):#0{10}x}"[-4:] + " " + f"{(int(format((player_key ^ enemy_key ^ data5), '#034b')[2:], 2)):#0{10}x}"[0:6])
+            #if ((literal_eval(hex(int(format((player_key ^ enemy_key ^ data1), '#034b')[2:], 2))[0:2] + hex(int(format((player_key ^ enemy_key ^ data1), '#034b')[2:], 2))[-4:] )) < 440):
+                #print("VALID MON")
+print("Done")
